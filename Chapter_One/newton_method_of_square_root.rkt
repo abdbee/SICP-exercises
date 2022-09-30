915ad654eed7b738547a956f5645c989986e3eee
Abdulbasit sadiq <76534194+abdbee@users.noreply.github.com>
2022-09-30 22:55:57 +0100

    exercise 7

--- Chapter_One/newton_method_of_square_root.rkt
+++ Chapter_One/newton_method_of_square_root.rkt
@@ -1,10 +1,34 @@
 #lang sicp
 
-(define (quotient guess x) (/ x guess))
-(define (newguess oldguess x) (/ (+ (quotient oldguess x) oldguess) x))
+;; ----------- implementation one ----------------
+(define (average guess x) (/ (+ guess x) 2))
+(define (newguess oldguess x) (average oldguess (/ x oldguess)))
 
 
 (define (error newguess x) (abs (- (* newguess newguess ) x)))
 
 (define (sqrt guess x)
-  (if (< (error(newguess guess x) x) 0.001) (newguess guess x) (sqrt (newguess guess x) x)))
+  (if (< (error(newguess guess x) x) (*(/ 1 10000000000) guess)) guess (sqrt (newguess guess x) x)))
+
+
+;; ---------------- implementation two ----------------
+
+(define (good-enough? guess x)
+  (< (abs (- (* guess guess) x)) (*(/ 1 10000000000) guess) ))   
+
+(define (average2 x y)
+  (/ (+ x y) 2))
+(define (improve guess x)
+    (average2 guess (/ x guess)))
+
+(define ( sqrt-of guess x)
+    (if (good-enough? guess x)
+       guess
+       (sqrt-of (improve guess x) 
+                 x))
+)
+
+; try changing the error margin to multiple fractions of the guess, and see how this affects the accuracy of the result for smaller and larger values
+(sqrt 1 25)
+
+(sqrt-of 1 250000)
